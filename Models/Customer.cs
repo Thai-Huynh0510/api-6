862eaf3874cf6ce3937453b4b3ce43b8b642542b
Thai (Rafe) Huynh <91857947+Thai-Huynh0510@users.noreply.github.com>
2023-01-26 18:17:24 -0500

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MyfirstAPI.Models
+
+{
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public Email? Email { get; set; }
+    }
+}
---
+++ Models/MyFristAPIDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using MyfirstAPI.Models;
+
+namespace MyfirstAPI.Models
+{
+    public class MyFirstAPIDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public MyFirstAPIDBContext(DbContextOptions<MyFirstAPIDBContext> options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("CustomerDataService");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Email> Emails { get; set; } = null!;
+    }
+}
